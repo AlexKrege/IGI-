47ef33737c5f1c119136ec5e5d2cbae560ae7898
AlexKrege <kregel.va@gmail.com>
2026-05-28 00:22:33 +0300

    new renarks

--- IGI/IGI-LR5/realt_agency/config/settings.py
+++ IGI/IGI-LR5/realt_agency/config/settings.py
@@ -82,7 +82,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = '/app/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
