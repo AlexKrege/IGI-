8732d2fa3d91628e11c6a627a82041132b9a6704
AlexKrege <kregel.va@gmail.com>
2026-06-04 01:13:11 +0300

    finaly version3

--- IGI/IGI-LR5/realt_agency/config/settings.py
+++ IGI/IGI-LR5/realt_agency/config/settings.py
@@ -1,3 +1,129 @@
+# import os
+# from pathlib import Path
+# from dotenv import load_dotenv
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# load_dotenv(BASE_DIR / '.env')
+
+# SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fake-key')
+# # DEBUG = os.getenv('DEBUG', '1') == '1'
+# # ALLOWED_HOSTS = ['*']
+# # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = True
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+
+# TIME_ZONE = 'Europe/Minsk'
+# USE_TZ = True
+
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     # приложения проекта
+#     'accounts',
+#     'agency',
+#     'api',
+#     'news',
+#     'properties',
+#     'reviews',
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     # 'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'agency.middleware.TimezoneMiddleware',
+# ]
+
+# ROOT_URLCONF = 'config.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [BASE_DIR / 'templates'],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#                 'agency.context_processors.global_context',
+#             ],
+#         },
+#     },
+# ]
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+#         'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+# if os.getenv('DB_ENGINE') == 'django.db.backends.postgresql':
+#     DATABASES['default'].update({
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     })
+
+# AUTH_USER_MODEL = 'accounts.User'
+# LOGIN_URL = '/accounts/login/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/'
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/app/media'
+
+# # LOGGING = {
+# #     'version': 1,
+# #     'disable_existing_loggers': False,
+# #     'formatters': {
+# #         'verbose': {'format': '{levelname} {asctime} {module} {message}', 'style': '{'},
+# #     },
+# #     'handlers': {
+# #         'file': {
+# #             'level': 'INFO',
+# #             'class': 'logging.FileHandler',
+# #             'filename': BASE_DIR / 'logs/debug.log',
+# #             'formatter': 'verbose',
+# #         },
+# #         'console': {'class': 'logging.StreamHandler'},
+# #     },
+# #     'root': {'handlers': ['console', 'file'], 'level': 'INFO'},
+# # }
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'root': {
+#         'handlers': ['console'],
+#         'level': 'INFO',
+#     },
+# }
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -5,16 +131,9 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fake-key')
-# DEBUG = os.getenv('DEBUG', '1') == '1'
-# ALLOWED_HOSTS = ['*']
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = True
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-
-
-TIME_ZONE = 'Europe/Minsk'
-USE_TZ = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -23,7 +142,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # приложения проекта
+    'cloudinary',
+    'cloudinary_storage',
     'accounts',
     'agency',
     'api',
@@ -34,7 +154,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # для статики
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -65,60 +185,35 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if os.getenv('DB_ENGINE') == 'django.db.backends.postgresql':
-    DATABASES['default'].update({
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    })
 
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Статика (CSS, JS) – будет собираться и отдаваться через Whitenoise
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
+# Cloudinary для медиа (картинки, загружаемые через админку)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {'format': '{levelname} {asctime} {module} {message}', 'style': '{'},
-#     },
-#     'handlers': {
-#         'file': {
-#             'level': 'INFO',
-#             'class': 'logging.FileHandler',
-#             'filename': BASE_DIR / 'logs/debug.log',
-#             'formatter': 'verbose',
-#         },
-#         'console': {'class': 'logging.StreamHandler'},
-#     },
-#     'root': {'handlers': ['console', 'file'], 'level': 'INFO'},
-# }
-
+# Упрощённое логирование
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'root': {'handlers': ['console'], 'level': 'INFO'},
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
